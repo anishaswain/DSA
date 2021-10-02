5e0c11f63a8cb876712d0f49d8102f9eeae63333
krishna <krish0cyber@gail.com>
2021-10-02 13:06:26 +0530

    added java bubble sort

---
+++ Sort/Java/BubbleSort.java
@@ -0,0 +1,28 @@
+
+public class BubbleSort {
+
+    public static void sort(int[] arr){
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++)
+            for (int j = 0; j < n-i-1; j++)
+                if (arr[j] > arr[j+1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+    }
+    public static void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        BubbleSort obj = new BubbleSort();
+        int[] arr = new int[]{23, 4, 12, 0, -1, 200};
+        obj.sort(arr);
+        printArray(arr);
+    }
+}
